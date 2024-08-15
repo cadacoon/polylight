75b2a23d62ce7baaa3dd982125a13a03b146c15a
valaphee <32491319+valaphee@users.noreply.github.com>
2024-08-15 10:58:47 +0200

    Tidy-up color and main

--- src/color.rs
+++ src/color.rs
@@ -1,150 +1,132 @@
-use parry3d::na::{Matrix3, Vector3};
+use std::simd::prelude::*;
 
-pub const SPD_SAMPLES: usize = 16;
-pub const SPD_START: f32 = 380.0;
-pub const SPD_END: f32 = 750.0;
-pub const SPD_INTERVAL: f32 = (SPD_END - SPD_START) / SPD_SAMPLES as f32;
+pub struct Spd<'a>(pub &'a [(f32, f32)]);
 
-pub struct Spd(pub [f32; SPD_SAMPLES]);
+pub struct Xyz(f32x4);
 
-pub struct Vspd<'a>(pub &'a [(f32, f32)]);
-
-pub struct Xyz(Vector3<f32>);
-
-impl From<Spd> for Xyz {
+impl<'a> From<Spd<'a>> for Xyz {
     fn from(value: Spd) -> Self {
-        let mut wavelength = SPD_START;
-        let mut xyz = Vector3::new(0.0, 0.0, 0.0);
-        for power in value.0 {
-            let x = xfit_1931(wavelength);
-            let y = yfit_1931(wavelength);
-            let z = zfit_1931(wavelength);
-            wavelength += SPD_INTERVAL;
-            xyz += Vector3::new(x, y, z) * power;
-        }
-        Xyz(xyz)
-    }
-}
-
-impl<'a> From<Vspd<'a>> for Xyz {
-    fn from(value: Vspd) -> Self {
-        let mut xyz = Vector3::new(0.0, 0.0, 0.0);
-        for &(wavelength, power) in value.0 {
-            let x = xfit_1931(wavelength);
-            let y = yfit_1931(wavelength);
-            let z = zfit_1931(wavelength);
-            xyz += Vector3::new(x, y, z) * power;
+        let mut xyz = f32x4::default();
+        for &(wavelength, radiation) in value.0 {
+            xyz += f32x4::splat(radiation)
+                * f32x4::from([
+                    xfit_1931(wavelength),
+                    yfit_1931(wavelength),
+                    zfit_1931(wavelength),
+                    0.0,
+                ]);
         }
         Xyz(xyz)
     }
 }
 
 impl Xyz {
-    pub fn rgb(self, color_space: ColorSpace) -> Vector3<f32> {
-        let rgb = color_space.0 * self.0;
-        let w = rgb.min().min(0.0);
+    pub fn to_linear(self, color_space: ColorSpace) -> [f32; 4] {
+        let mut rgb = simd_swizzle!(color_space.xyz, [0, 4, 8, 12]) * f32x4::splat(self.0[0]);
+        rgb += simd_swizzle!(color_space.xyz, [1, 5, 9, 13]) * f32x4::splat(self.0[1]);
+        rgb += simd_swizzle!(color_space.xyz, [2, 6, 10, 14]) * f32x4::splat(self.0[2]);
+        return rgb.to_array();
+    }
 
-        //let mut rgb = rgb - Vector3::new(w, w, w);
-
-        return rgb;
+    pub fn to_nonlinear(self, color_space: ColorSpace) -> [f32; 4] {
+        let mut rgb = simd_swizzle!(color_space.xyz, [0, 4, 8, 12]) * f32x4::splat(self.0[0]);
+        rgb += simd_swizzle!(color_space.xyz, [1, 5, 9, 13]) * f32x4::splat(self.0[1]);
+        rgb += simd_swizzle!(color_space.xyz, [2, 6, 10, 14]) * f32x4::splat(self.0[2]);
+        for x in rgb.as_mut_array() {
+            *x = (color_space.oetf)(*x);
+        }
+        return rgb.to_array();
     }
 }
 
-pub const REC709: ColorSpace = ColorSpace::new([0.3127, 0.3290], [[0.64, 0.33], [0.30, 0.60], [0.15, 0.06]]);
-pub const CIE: ColorSpace = ColorSpace::new([0.33333333, 0.33333333], [[0.7355, 0.2645], [0.2658, 0.7243], [0.1669, 0.0085]]);
-
-pub struct ColorSpace(Matrix3<f32>);
+pub struct ColorSpace {
+    xyz: f32x16,
+    oetf: fn(f32) -> f32,
+}
 
 impl ColorSpace {
-    const fn new(white: [f32; 2], primaries: [[f32; 2]; 3]) -> Self {
-        let xr = primaries[0][0];
-        let yr = primaries[0][1];
-        let xg = primaries[1][0];
-        let yg = primaries[1][1];
-        let xb = primaries[2][0];
-        let yb = primaries[2][1];
-        let xw = white[0];
-        let yw = white[1];
-        let zr = 1.0 - (xr + yr);
-        let zg = 1.0 - (xg + yg);
-        let zb = 1.0 - (xb + yb);
-        let zw = 1.0 - (xw + yw);
+    const fn new(r: [f32; 2], g: [f32; 2], b: [f32; 2], w: [f32; 2], oetf: fn(f32) -> f32) -> Self {
+        let r2 = 1.0 - (r[0] + r[1]);
+        let g2 = 1.0 - (g[0] + g[1]);
+        let b2 = 1.0 - (b[0] + b[1]);
+        let w2 = 1.0 - (w[0] + w[1]);
 
-        let mut rx = (yg * zb) - (yb * zg);
-        let mut ry = (xb * zg) - (xg * zb);
-        let mut rz = (xg * yb) - (xb * yg);
-        let mut gx = (yb * zr) - (yr * zb);
-        let mut gy = (xr * zb) - (xb * zr);
-        let mut gz = (xb * yr) - (xr * yb);
-        let mut bx = (yr * zg) - (yg * zr);
-        let mut by = (xg * zr) - (xr * zg);
-        let mut bz = (xr * yg) - (xg * yr);
-
-        let rw = ((rx * xw) + (ry * yw) + (rz * zw)) / yw;
-        let gw = ((gx * xw) + (gy * yw) + (gz * zw)) / yw;
-        let bw = ((bx * xw) + (by * yw) + (bz * zw)) / yw;
+        let mut rx = (g[1] * b2) - (b[1] * g2);
+        let mut ry = (b[0] * g2) - (g[0] * b2);
+        let mut rz = (g[0] * b[1]) - (b[0] * g[1]);
+        let rw = ((rx * w[0]) + (ry * w[1]) + (rz * w2)) / w[1];
         rx = rx / rw;
         ry = ry / rw;
         rz = rz / rw;
+
+        let mut gx = (b[1] * r2) - (r[1] * b2);
+        let mut gy = (r[0] * b2) - (b[0] * r2);
+        let mut gz = (b[0] * r[1]) - (r[0] * b[1]);
+        let gw = ((gx * w[0]) + (gy * w[1]) + (gz * w2)) / w[1];
         gx = gx / gw;
         gy = gy / gw;
         gz = gz / gw;
+
+        let mut bx = (r[1] * g2) - (g[1] * r2);
+        let mut by = (g[0] * r2) - (r[0] * g2);
+        let mut bz = (r[0] * g[1]) - (g[0] * r[1]);
+        let bw = ((bx * w[0]) + (by * w[1]) + (bz * w2)) / w[1];
         bx = bx / bw;
         by = by / bw;
         bz = bz / bw;
 
-        Self(Matrix3::new(
-            rx, ry, rz,
-            gx, gy, gz,
-            bx, by, bz,
-        ))
+        Self {
+            xyz: f32x16::from_array([
+                rx, ry, rz, 0.0, gx, gy, gz, 0.0, bx, by, bz, 0.0, 0.0, 0.0, 0.0, 1.0,
+            ]),
+            oetf,
+        }
     }
 }
 
-// Wyman, C., Sloan P., & Shirley P. (2013). Journal of Computer Graphics Techniques: Simple Analytic Approximations to the CIE XYZ Color Matching Functions
+// Wyman, Chris; Sloan, Peter-Pike; Shirley, Peter (July 12, 2013). "Simple Analytic Approximations to the CIE XYZ Color Matching Functions". Journal of Computer Graphics Techniques. 2 (2): 1-11. ISSN 2331-7418.
 fn xfit_1931(wave: f32) -> f32 {
-    let t1 = (wave - 442.0) * if wave < 442.0 {
-        0.0624
-    } else {
-        0.0374
-    };
-    let t2 = (wave - 599.8) * if wave < 599.8 {
-        0.0264
-    } else {
-        0.0323
-    };
-    let t3 = (wave - 501.1) * if wave < 501.1 {
-        0.0490
-    } else {
-        0.0382
-    };
-    0.362 * (-0.5 * t1 * t1).exp() + (1.056 * (-0.5 * t2 * t2).exp()) - (0.065 * (-0.5 * t3 * t3).exp())
+    let t1 = (wave - 442.0) * if wave < 442.0 { 0.0624 } else { 0.0374 };
+    let t2 = (wave - 599.8) * if wave < 599.8 { 0.0264 } else { 0.0323 };
+    let t3 = (wave - 501.1) * if wave < 501.1 { 0.0490 } else { 0.0382 };
+    0.362 * (-0.5 * t1 * t1).exp() + (1.056 * (-0.5 * t2 * t2).exp())
+        - (0.065 * (-0.5 * t3 * t3).exp())
 }
 
 fn yfit_1931(wave: f32) -> f32 {
-    let t1 = (wave - 568.8) * if wave < 568.8 {
-        0.0213
-    } else {
-        0.0247
-    };
-    let t2 = (wave - 530.9) * if wave < 530.9 {
-        0.0613
-    } else {
-        0.0322
-    };
+    let t1 = (wave - 568.8) * if wave < 568.8 { 0.0213 } else { 0.0247 };
+    let t2 = (wave - 530.9) * if wave < 530.9 { 0.0613 } else { 0.0322 };
     0.821 * (-0.5 * t1 * t1).exp() + (0.286 * (-0.5 * t2 * t2).exp())
 }
 
 fn zfit_1931(wave: f32) -> f32 {
-    let t1 = (wave - 437.0) * if wave < 437.0 {
-        0.0845
-    } else {
-        0.0278
-    };
-    let t2 = (wave - 459.0) * if wave < 459.0 {
-        0.0385
-    } else {
-        0.0725
-    };
+    let t1 = (wave - 437.0) * if wave < 437.0 { 0.0845 } else { 0.0278 };
+    let t2 = (wave - 459.0) * if wave < 459.0 { 0.0385 } else { 0.0725 };
     1.217 * (-0.5 * t1 * t1).exp() + (0.681 * (-0.5 * t2 * t2).exp())
 }
+
+const D65: [f32; 2] = [0.3127, 0.3290];
+
+// ITU-R BT.709
+fn bt709_oetf(value: f32) -> f32 {
+    if value < 0.018 {
+        4.5 * value
+    } else {
+        1.099 * value.powf(0.45) - 0.099
+    }
+}
+
+pub const BT709: ColorSpace =
+    ColorSpace::new([0.64, 0.33], [0.30, 0.60], [0.15, 0.06], D65, bt709_oetf);
+
+// W3C sRGB
+fn srgb_oetf(value: f32) -> f32 {
+    if value <= 0.00304 {
+        12.92 * value
+    } else {
+        1.055 * value.powf(1.0 / 2.4) - 0.055
+    }
+}
+
+pub const SRGB: ColorSpace =
+    ColorSpace::new([0.64, 0.33], [0.30, 0.60], [0.15, 0.06], D65, srgb_oetf);
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,17 @@
-#![feature(const_fn_floating_point_arithmetic)]
+#![feature(const_fn_floating_point_arithmetic, portable_simd)]
 
 mod color;
 
-use std::ops::Index;
-use std::time::Instant;
-use eframe::{egui, Frame};
-use eframe::egui::Color32;
-use egui_plot::{Line, Plot, PlotPoints};
-use parry3d::math::{Isometry, Point, Vector};
-use parry3d::query::{Ray, RayCast};
-use parry3d::shape::Ball;
-use crate::color::{CIE, REC709, Vspd, Xyz};
+use eframe::egui;
 
 fn main() -> eframe::Result {
     eframe::run_native(
         "PolyLight",
         eframe::NativeOptions {
-            viewport: egui::ViewportBuilder::default().with_inner_size([800.0, 600.0]),
+            viewport: egui::ViewportBuilder::default().with_inner_size([512.0, 512.0]),
             ..Default::default()
         },
-        Box::new(|cc| {
-            Ok(Box::<App>::default())
-        }),
+        Box::new(|_cc| Ok(Box::<App>::default())),
     )
 }
 
@@ -35,102 +25,23 @@ impl Default for App {
     fn default() -> Self {
         Self {
             output: None,
-            output_image: egui::ColorImage::new([830 - 360, 512], egui::Color32::BLACK),
+            output_image: egui::ColorImage::new([512, 512], egui::Color32::BLACK),
             update: true,
         }
     }
 }
 
 impl eframe::App for App {
-    fn update(&mut self, ctx: &egui::Context, frame: &mut Frame) {
-        let output = self.output.get_or_insert_with(|| ctx.load_texture("output", self.output_image.clone(), Default::default()));
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        let output = self.output.get_or_insert_with(|| {
+            ctx.load_texture("output", self.output_image.clone(), Default::default())
+        });
         if self.update {
-            let time = Instant::now();
-            let camera = Camera::new(Point::origin(), Point::new(1.0, 0.0, 0.0 ), Vector::new(0.0, 1.0, 0.0),20.0, (self.output_image.width() as f32) / (self.output_image.height() as f32));
-            let ball = Ball::new(1.0);
-            let isom = Isometry::translation(10.0, 0.0, 0.0);
-
-            let mut i = 0;
-            for x in 0..self.output_image.height() {
-                for y in 0..self.output_image.width() {
-                    /*let u = x as f32 / self.output_image.width() as f32;
-                    let v = y as f32 / self.output_image.height() as f32;
-                    let r = camera.get_ray(u, v);
-                    if let Some(c) = ball.cast_ray_and_get_normal(&isom, &r, f32::MAX, true) {
-                        self.output_image.pixels[i] = egui::Color32::from_rgb(((c.normal.x + 1.0) * 128.0) as u8, ((c.normal.y + 1.0) * 128.0) as u8, ((c.normal.z + 1.0) * 128.0) as u8);
-                    }*/
-                    let x = y as f32 + 360.0;
-                    let xyz = Xyz::from(Vspd(&[(x, 1.0)]));
-                    let rgb = xyz.rgb(REC709);
-
-                    self.output_image.pixels[i] = Color32::from_rgb((rgb.x * 256.0).min(255.0) as u8, (rgb.y * 256.0).min(255.0) as u8, (rgb.z * 256.0).min(255.0) as u8);
-                    i += 1;
-                }
-            }
             output.set(self.output_image.clone(), Default::default());
-            println!("{:?}", time.elapsed());
-
             self.update = false;
         }
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.add(egui::Image::new((output.id(), output.size_vec2())).shrink_to_fit());
-            /*let xa: PlotPoints = (360..830).map(|i| {
-                let x = i as f32;
-                let xyz = Xyz::from(Vspd(&[(x, 1.0)]));
-                [x as f64, xyz.rgb(CIE).x as f64]
-            }).collect();
-            let xb: PlotPoints = (360..830).map(|i| {
-                let x = i as f32;
-                let xyz = Xyz::from(Vspd(&[(x, 1.0)]));
-                [x as f64, xyz.rgb(CIE).y as f64]
-            }).collect();
-            let xc: PlotPoints = (360..830).map(|i| {
-                let x = i as f32;
-                let xyz = Xyz::from(Vspd(&[(x, 1.0)]));
-                [x as f64, xyz.rgb(CIE).z as f64]
-            }).collect();
-            let linea = Line::new(xa).color(Color32::RED);
-            let lineb = Line::new(xb).color(Color32::GREEN);
-            let linec = Line::new(xc).color(Color32::BLUE);
-            Plot::new("cie").view_aspect(2.0).show(ui, |pui| {
-                pui.line(linea);
-                pui.line(lineb);
-                pui.line(linec);
-            });*/
         });
-    }
-}
-
-struct Camera {
-    origin: Point<f32>,
-    corner: Point<f32>,
-    horizontal: Vector<f32>,
-    vertical: Vector<f32>,
-}
-
-impl Camera {
-    fn new(origin: Point<f32>, look_at: Point<f32>, vup: Vector<f32>, vfov: f32, aspect: f32) -> Self {
-        let theta = vfov.to_radians();
-        let half_height = (theta / 2.0).tan();
-        let half_width = aspect * half_height;
-
-        let w = (origin - look_at).normalize();
-        let u = vup.cross(&w).normalize();
-        let v = w.cross(&u);
-
-        let corner = origin - (u * half_width) - (v * half_height) - w;
-        let horizontal = u * 2.0 * half_width;
-        let vertical = v * 2.0 * half_height;
-
-        Self {
-            origin,
-            corner,
-            horizontal,
-            vertical,
-        }
-    }
-
-    fn get_ray(&self, u: f32, v: f32) -> Ray {
-        Ray::new(self.origin, self.corner + (self.horizontal * u) + (self.vertical * v) - self.origin)
     }
 }
